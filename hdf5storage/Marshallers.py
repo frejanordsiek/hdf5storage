69f745af2a3b8bb8fc43e7830cc814bdd7660bda
Freja Nordsiek <fnordsie@gmail.com>
2014-01-12 00:53:33 -0500

    Added minimal docstring for Marshallers module.

--- hdf5storage/Marshallers.py
+++ hdf5storage/Marshallers.py
@@ -24,6 +24,10 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+""" Module for the classes to marshall Python types to/from file.
+
+"""
+
 import numpy as np
 import h5py
 
