48e49760ef36686700919a818434116d45ceb5d5
Freja Nordsiek <fnordsie@gmail.com>
2015-12-27 15:24:56 +0100

    Incremented version to 0.1.11.

--- hdf5storage/__init__.py
+++ hdf5storage/__init__.py
@@ -28,11 +28,11 @@ This is the hdf5storage package, a Python package to read and write
 python data types to HDF5 (Heirarchal Data Format) files beyond just
 Numpy types.
 
-Version 0.1.10
+Version 0.1.11
 
 """
 
-__version__ = "0.1.10"
+__version__ = "0.1.11"
 
 import sys
 import os
