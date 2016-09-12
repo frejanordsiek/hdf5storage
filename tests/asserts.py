2b5725fd605f5d5a3443183e354a4c8c001a32a5
Freja Nordsiek <fnordsie@gmail.com>
2016-09-12 21:48:07 +0200

    Made it so that RuntimeWarnings are suppressed in tests where numpy.isnan or numpy.isinf would be executed on floating point and complex types since some NaN values result in that warning.

--- tests/asserts.py
+++ tests/asserts.py
@@ -26,6 +26,7 @@
 
 import sys
 import collections
+import warnings
 
 import numpy as np
 import numpy.testing as npt
@@ -54,18 +55,22 @@ def assert_equal(a, b):
             for index in range(0, len(a)):
                 assert_equal(a[index], b[index])
     elif not isinstance(b, (np.generic, np.ndarray)):
-        if isinstance(b, complex):
-            assert a.real == b.real \
-                or np.all(np.isnan([a.real, b.real]))
-            assert a.imag == b.imag \
-                or np.all(np.isnan([a.imag, b.imag]))
-        else:
-            assert a == b or np.all(np.isnan([a, b]))
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore', RuntimeWarning)
+            if isinstance(b, complex):
+                assert a.real == b.real \
+                    or np.all(np.isnan([a.real, b.real]))
+                assert a.imag == b.imag \
+                    or np.all(np.isnan([a.imag, b.imag]))
+            else:
+                assert a == b or np.all(np.isnan([a, b]))
     else:
         assert a.dtype == b.dtype
         assert a.shape == b.shape
         if b.dtype.name != 'object':
-            npt.assert_equal(a, b)
+            with warnings.catch_warnings():
+                warnings.simplefilter('ignore', RuntimeWarning)
+                npt.assert_equal(a, b)
         else:
             for index, x in np.ndenumerate(a):
                 assert_equal(a[index], b[index])
@@ -150,7 +155,9 @@ def assert_equal_none_format(a, b):
                 if np.prod(a.shape) == 1:
                     a = np.squeeze(a)
                     b = np.squeeze(b)
-                npt.assert_equal(a, b)
+                with warnings.catch_warnings():
+                    warnings.simplefilter('ignore', RuntimeWarning)
+                    npt.assert_equal(a, b)
         else:
             assert a.dtype == b.dtype
             assert a.shape == b.shape
@@ -239,7 +246,9 @@ def assert_equal_matlab_format(a, b):
                     c = np.atleast_2d(b)
                     assert a.dtype == c.dtype
                     assert a.shape == c.shape
-                    npt.assert_equal(a, c)
+                    with warnings.catch_warnings():
+                        warnings.simplefilter('ignore', RuntimeWarning)
+                        npt.assert_equal(a, c)
             else:
                 c = np.atleast_2d(b)
                 # An empty complex number gets turned into a real
@@ -254,7 +263,9 @@ def assert_equal_matlab_format(a, b):
                 if b.dtype.names is None and a.dtype.names is None:
                     assert a.dtype == c.dtype
                     assert a.shape == c.shape
-                    npt.assert_equal(a, c)
+                    with warnings.catch_warnings():
+                        warnings.simplefilter('ignore', RuntimeWarning)
+                        npt.assert_equal(a, c)
                 else:
                     assert a.dtype.names is not None
                     assert b.dtype.names is not None
@@ -317,4 +328,6 @@ def assert_equal_from_matlab(a, b):
             for index, x in np.ndenumerate(a):
                 assert_equal_from_matlab(a[k][index], b[k][index])
     else:
-        npt.assert_equal(a, b)
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore', RuntimeWarning)
+            npt.assert_equal(a, b)
