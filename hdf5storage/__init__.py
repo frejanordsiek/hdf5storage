b426f980810c178f3c118b2ea7a77bebb165ad1b
Randy Pittman <randallpittman@outlook.com>
2020-03-02 23:37:19 +0000

    Always include mode when opening File with h5py
    Addresses #82.

--- hdf5storage/__init__.py
+++ hdf5storage/__init__.py
@@ -1687,7 +1687,7 @@ def writes(mdict, filename='data.h5', truncate_existing=False,
     # can be closed if any errors happen (the error is re-raised).
     f = None
     try:
-        f = h5py.File(filename)
+        f = h5py.File(filename, mode='a')
 
         # Go through each element of towrite and write them.
         for groupname, targetname, data in towrite:
