5758a3b98ad9a7101fffdbb0a5b47c8794543377
Freja Nordsiek <fnordsie@posteo.net>
2020-04-19 21:49:55 +0200

    Updated the example marshaller plugin to use PEP 518 and got rid of Python 2.7 bits.

--- examples/example_hdf5storage_marshaller_plugin/setup.py
+++ examples/example_hdf5storage_marshaller_plugin/setup.py
@@ -1,5 +1,28 @@
-import ez_setup
-ez_setup.use_setuptools()
+# Copyright (c) 2017-2020, Freja Nordsiek
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from setuptools import setup
 
@@ -20,8 +43,7 @@ setup(name='example_hdf5storage_marshaller_plugin',
       keywords='hdf5storage',
       zip_safe=True,
       classifiers=[
-          "Programming Language :: Python :: 2.7",
-          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3 :: Only",
           "Development Status :: 3 - Alpha",
           "License :: OSI Approved :: BSD License",
           "Operating System :: OS Independent",
